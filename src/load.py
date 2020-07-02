a26f5696387ec7007d47e2b227a458b071a22bc6
Yang Guo <gy@fir.im>
2020-07-03 00:09:20 +0200

    script to load secret

---
+++ src/load.py
@@ -0,0 +1,29 @@
+import sys
+import json
+from flowci import client, domain
+
+SecretName = client.GetVar('SIGN_SECRET')
+
+api = client.Client()
+secret = api.getCredential(SecretName)
+
+if secret is None:
+    sys.exit('unable to load secret' + SecretName)
+
+try:
+    keyStoreName = secret['keyStoreFileName']
+    path = api.downloadSecretFile(SecretName, keyStoreName)
+
+    if path is None:
+        sys.exit('unable to load keystore file' + keyStoreName)
+
+    print(path) # key path
+    print(secret['keyStorePassword']['data'])  # ks pw
+    print(secret['keyAlias']) # key alias
+    print(secret['keyPassword']['data']) # key pw
+except:
+    sys.exit('invalid secret')
+
+
+
+
